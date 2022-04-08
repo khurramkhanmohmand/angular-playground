b1aae341e8ec9d424fcd44f8d50b3da6b3865b2d
khurram <kkh@facelift-bbt.com>
2022-04-08 16:29:30 +0500

    update with home and header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ChapterRubabComponent } from './chapter-rubab/chapter-rubab.component';
 import { ChapterKhurramComponent } from './chapter-khurram/chapter-khurram.component';
 import { ChapterAdnanComponent } from './chapter-adnan/chapter-adnan.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { ChapterAdnanComponent } from './chapter-adnan/chapter-adnan.component';
   ],
   imports: [
     RouterModule.forRoot([
+      {path: '', component: HomeComponent},
       {path: 'rubab-work', component: ChapterRubabComponent},
       {path: 'khurram-work', component: ChapterKhurramComponent},
       {path: 'adnan-work', component: ChapterAdnanComponent}
