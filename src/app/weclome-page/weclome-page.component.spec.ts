40610cd3dd3d67b1e0c66d9797f6efd5a8b1e2af
Adnan Ali <i.adnanali@outlook.com>
2022-04-08 15:16:16 +0500

    app-comp cleanup, welcome-page component added

---
+++ src/app/weclome-page/weclome-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WeclomePageComponent } from './weclome-page.component';
+
+describe('WeclomePageComponent', () => {
+  let component: WeclomePageComponent;
+  let fixture: ComponentFixture<WeclomePageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WeclomePageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WeclomePageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
