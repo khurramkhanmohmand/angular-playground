991f7bc18d9610ddd4a4776be451bcc8b70424f7
Adnan Ali <i.adnanali@outlook.com>
2022-04-08 15:47:16 +0500

    app routing refined, toolbar style dirty fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { ChapterRubabComponent } from './chapter-rubab/chapter-rubab.component';
 import { ChapterKhurramComponent } from './chapter-khurram/chapter-khurram.component';
 import { ChapterAdnanComponent } from './chapter-adnan/chapter-adnan.component';
-
+import { WeclomePageComponent } from './weclome-page/weclome-page.component';
 
 @NgModule({
-  declarations:[
-    ChapterRubabComponent,
-    ChapterKhurramComponent,
-    ChapterAdnanComponent
-  ],
   imports: [
     RouterModule.forRoot([
       {path: 'rubab-work', component: ChapterRubabComponent},
       {path: 'khurram-work', component: ChapterKhurramComponent},
-      {path: 'adnan-work', component: ChapterAdnanComponent}
+      {path: 'adnan-work', component: ChapterAdnanComponent},
+      {path: '', component: WeclomePageComponent}
     ])
   ],
   providers: [],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChapterAdnanComponent } from './chapter-adnan/chapter-adnan.component';
+import { ChapterKhurramComponent } from './chapter-khurram/chapter-khurram.component';
+import { ChapterRubabComponent } from './chapter-rubab/chapter-rubab.component';
 import { WeclomePageComponent } from './weclome-page/weclome-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeclomePageComponent
+    WeclomePageComponent,
+    ChapterAdnanComponent,
+    ChapterKhurramComponent,
+    ChapterRubabComponent
   ],
   imports: [
     BrowserModule,
